4a9a201b6795fc9535e097122a5aabae40b3ab9f
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-03-16 03:05:52 +0700

    Add files via upload

---
+++ QLBanSach/src/GUI/QLBanSachGUI.java
@@ -0,0 +1,52 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class QLBanSachGUI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					QLBanSachGUI frame = new QLBanSachGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public QLBanSachGUI()
+	{
+		setTitle("Quản lý bán giày");
+		initComponents();
+	}
+	/**
+	 * Create the frame.
+	 */
+	public void initComponents() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+	}
+
+}
---
+++ QLBanSach/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module QLBanSach {
+	requires java.desktop;
+}
