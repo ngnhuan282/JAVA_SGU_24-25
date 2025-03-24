ba21ce6f92dcb5be5785b0711780c8efb6404205
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-03-24 22:56:38 +0700

    Create SanPhamBUS.java

---
+++ QLBanGiay/src/BUS/SanPhamBUS.java
@@ -0,0 +1,29 @@
+package BUS;
+
+import java.util.ArrayList;
+
+import DTO.SanPhamDTO;
+
+public class SanPhamBUS {
+	private ArrayList<SanPhamDTO> dssp;
+	
+	public SanPhamBUS()
+	{
+		
+	}
+	
+	public void listSP()
+	{
+		
+	}
+	
+	public void addSP()
+	{
+		
+	}
+	
+	public void deleteSP()
+	{
+		
+	}
+}
